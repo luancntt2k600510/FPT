2015e03e201541dfec256039f1e06103ae2823c9
luancntt2k600510 <72446058+luancntt2k600510@users.noreply.github.com>
2023-03-02 18:42:43 +0700

    Add files via upload

---
+++ Boostrap/LuanND3_CSS-FW.M.A103/main.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById("mySidebar").style.width = "15%";
+  document.getElementById("main").style.marginRight = "15%";
+  document.getElementById("Openbtn").style.visibility = "hidden";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginRight = "0";
+  document.getElementById("Openbtn").style.visibility = "visible";
+}
